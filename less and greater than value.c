e70e322ee6a3df301bb8719cb41385c6eef79fc3
Aarti <103598054+aartisutar@users.noreply.github.com>
2022-06-16 20:52:09 +0530

    Add files via upload

---
+++ less and greater than value.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int A = 10;
+
+    if(A == 20)
+    {
+        printf("A is less than 20\n");
+    }
+    else if (A == 30)
+    {
+       printf("A is less than 30\n");
+
+    }
+    else if (A == 40)
+    {
+    printf("A is less than 40\n");
+
+    }
+    else {
+    printf("None of the values is matching\n");
+    }
+printf ("Exect values of a is : %d\n",A);
+getch();
+
+
+}
+
