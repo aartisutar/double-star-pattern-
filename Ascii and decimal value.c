f0c314c42e6652a7146db982ced0ef1cb1bd73a1
Aarti <103598054+aartisutar@users.noreply.github.com>
2022-06-16 20:41:59 +0530

    Add files via upload

---
+++ Ascii and decimal value.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   char ch ;
+   printf("Please Enter any Character : ");
+   scanf("%c",&ch);
+   printf("ASCII Value of = %d",ch);
+
+    getch();
+    return 0;
+}
+
+
